d49e9f803e9e722d0b63f04ebcfe1f4ba679688a
Simonius77 <114445380+Simonius77@users.noreply.github.com>
2022-09-26 23:40:31 +0200

    First commit

---
+++ Calculadora.java
@@ -0,0 +1,65 @@
+public class Calculadora {
+		public static void main(String[] args) {
+			System.out.println("Calculadora");
+			int entera = 392;
+			int enterb = 10;
+			int enterc = 10;
+			//int enterd = 2;
+			int resultado = 0;
+			double resultadoa=1.0;
+			//resultado=elMeuEntera+elMeuEnterb;
+			
+			System.out.println(resultadoa=4.0 / 2 * 9 / 2);
+			
+			System.out.println(resultadoa=2.5 * 2 + 8 / 5.0 + 10 / 3);
+			
+			System.out.println(resultadoa=12 / 7 * 4.4 * 2 / 4);
+			
+			System.out.println(resultadoa=4 * 3 / 8 + 2.5 * 2);
+			
+			System.out.println(resultadoa=(5 * 7.0 / 2 - 2.5) / 5 * 2);
+			
+			System.out.println(resultadoa=41 % 7 * 3 / 5 + 5 / 2 * 2.5);
+
+			System.out.println(resultadoa=10.0 / 2/4);
+			
+			System.out.println(resultadoa=8 / 5 + 13 / 2 / 3.0);			
+			
+			System.out.println(resultadoa=(2.5 + 3.5) / 2);		
+				
+			System.out.println(resultadoa=9 / 4 * 2.0 - 5/4);
+				
+			System.out.println(resultadoa=9 / 2.0 + 7 / 3 - 3.0 / 2);
+				
+			System.out.println(resultadoa=813 % 100 / 3 + 2.4);
+				
+			System.out.println(resultadoa=27 / 2 / 2.0 * (4.3 + 1.7) - 8 / 3);
+			
+			System.out.println(resultadoa=53 / 5 / (0.6 + 1.4) / 2 + 13 / 2);
+			
+			System.out.println(resultadoa=2 * 3 / 4 * 2 / 4.0 + 4.5 - 1);			
+				
+			System.out.println(resultadoa=89 % 10 / 4 * 2.0 / 5 + (1.5 + 1.0 / 2) * 2);
+				
+			//System.out.println(resultado=392 / 10 % 10 / 2);
+				
+			//System.out.println(resultado=8 * 2 - 7 / 4);
+				
+			//System.out.println(resultado=37 % 20 % 3 * 4);
+			
+			//System.out.println(resultado=17 % 10 / 4);
+		//	System.out.println(resultado);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+ }
